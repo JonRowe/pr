b098d1dc8979908e9876f64cd43dcc0989755e3c
Jon Rowe <hello@jonrowe.co.uk>
2013-03-25 16:32:23 +1100

    form inclusion ties things together

---
+++ lib/PR/form.rb
@@ -0,0 +1,13 @@
+module Form
+
+  def self.included other
+    other.send :extend, DSL
+  end
+
+  module DSL
+    def field name, options = {}
+      DefineAndRegister[ self, name, options ]
+    end
+  end
+
+end
---
+++ spec/unit/pr/form_spec.rb
@@ -0,0 +1,17 @@
+require 'pr/form'
+
+describe 'the PR Form' do
+  let(:form_klass) do
+    Class.new do
+      include Form
+    end
+  end
+
+  describe 'when included extends the other with its DSL' do
+    it 'sets up field to define fields' do
+      stub_const 'DefineAndRegister', double
+      DefineAndRegister.should_receive(:[]).with form_klass, :name, my: :options
+      form_klass.field :name, my: :options
+    end
+  end
+end
