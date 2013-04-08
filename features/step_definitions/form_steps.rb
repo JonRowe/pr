c18f56477e54df2557822cf4b8f0aabd354cf172
Jon Rowe <hello@jonrowe.co.uk>
2013-04-08 23:12:24 +1000

    setup for form from models

--- features/step_definitions/form_steps.rb
+++ features/step_definitions/form_steps.rb
@@ -6,6 +6,10 @@ Given "I have preloaded the form with data:" do |code|
   run_test code
 end
 
+When "I build my form from a model:" do |code|
+  run_test code
+end
+
 When /^I initialise (\w+)$/ do |klass|
   @form = fetch(klass).new
 end
